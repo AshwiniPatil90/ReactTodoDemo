a22f88e98cd2ebf3614097a3cf331c1b326876e5
Ashwini Patil <110224955+AshwiniPatil90@users.noreply.github.com>
2023-09-25 11:14:10 -0400

    Add files via upload
    Initial commits of React Todo app

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './App.css';
+import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
+
+function App() {
+  const [todos, setTodos] = useState([
+    { rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User one' },
+    { rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User two' },
+    { rowNumber: '3', rowDescription: 'Make dinner', rowAssigned: 'User one' },
+    { rowNumber: '4', rowDescription: 'Charge phone  battery', rowAssigned: 'User one' }
+  ])
+
+  const addTodo = (description, assigned) => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: description,
+        rowAssigned: assigned
+      }
+      // todos.push(newTodo);
+      setTodos(todos => [...todos, newTodo]);
+    }
+  }
+  return (
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
+          Your Todo's
+        </div>
+        <div className='card-body'>
+          <TodoTable todos={todos} />
+          <button className='btn btn-primary' onClick={addTodo}>
+            Add new todo
+          </button>
+          <NewTodoForm addTodo={addTodo} />
+        </div>
+      </div>
+    </div >
+  );
+}
+
+export default App;
---
+++ src/components/TodoTable.js
@@ -0,0 +1,24 @@
+import TodoRowItem from "./TodoRowItem";
+
+const TodoTable = (props) => {
+    return (
+        <table className='table table-hover'>
+            <thead>
+                <tr>
+                    <th scope='col'>#</th>
+                    <th scope='col'>Description</th>
+                    <th scope='col'>Assigned</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.todos.map(todo => (
+                    <TodoRowItem rowNumber={todo.rowNumber}
+                        rowDescription={todo.rowDescription}
+                        rowAssigned={todo.rowAssigned} />
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default TodoTable;
